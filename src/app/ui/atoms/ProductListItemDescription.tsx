f362f6f72de9ed646c886d65f786a6a3356fa25f
kaminskij <kaminskiqba@gmail.com>
2023-09-12 13:13:13 +0200

    Fix for wrong tax on product description page

--- src/app/ui/atoms/ProductListItemDescription.tsx
+++ src/app/ui/atoms/ProductListItemDescription.tsx
@@ -11,9 +11,9 @@ export const ProductListItemDescription = ({
 	return (
 		<div className="mt-2 flex justify-between">
 			<div>
-				<h1 className="text-sm font-semibold text-gray-700">
+				<h3 className="text-sm font-semibold text-gray-700">
 					{name}
-				</h1>
+				</h3>
 				<p className="text-sm text-gray-500">
 					<span className="sr-only">Kategoria:</span>
 					{category}
