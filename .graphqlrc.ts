4f4528354a1cec1a75f07cee3a6307eb7b219099
Jakub <kaminskiqba@gmail.com>
2023-09-18 13:51:03 +0200

    First Graphql query (#9)
    * First Graphql query
    
    * Fixes
    
    * IMport fixes
    
    * Graphql types fixes

---
+++ .graphqlrc.ts
@@ -0,0 +1,30 @@
+/* eslint-disable import/no-default-export */
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import { loadEnvConfig } from "@next/env";
+
+loadEnvConfig(process.cwd());
+
+const config: CodegenConfig = {
+	overwrite: true,
+	schema: process.env.GRAPHQL_URL,
+	documents: "src/graphql/*.graphql",
+	ignoreNoDocuments: true,
+	generates: {
+		"src/gql/": {
+			preset: "client",
+			plugins: [],
+			presetConfig: {
+				fragmentMasking: false,
+			},
+			config: {
+				defaultScalarType: "unknown",
+				useTypeImports: true,
+				skipTypename: true,
+				documentMode: "string",
+				enumAsTypes: true,
+			},
+		},
+	},
+};
+
+export default config;
