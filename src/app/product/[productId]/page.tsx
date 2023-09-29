9c15115e85a07109b69718a90cda6c57fda95126
kaminskij <kaminskiqba@gmail.com>
2023-09-29 11:06:52 +0200

    OG image test

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -13,6 +13,17 @@ export const generateMetadata = async ({
 	return {
 		title: product && product.name,
 		description: product && product.description,
+		openGraph: {
+			title: (product && product.name) || "",
+			images: [
+				{
+					url: (product && product.images[0]?.url) || "",
+					width: 800,
+					height: 600,
+					alt: (product && product.name) || "",
+				},
+			],
+		},
 	};
 };
 
