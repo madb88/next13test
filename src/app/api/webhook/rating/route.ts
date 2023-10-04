6f1a09da35cf562a2302c74c4511e5e3a8458fbe
Jakub <kaminskiqba@gmail.com>
2023-10-04 11:53:30 +0200

    Amendments for webhook (#20)

--- src/app/api/webhook/rating/route.ts
+++ src/app/api/webhook/rating/route.ts
@@ -17,7 +17,6 @@ export type ReviewType = {
 
 export async function POST(request: NextRequest): Promise<Response> {
 	const body = (await request.json()) as ReviewType;
-	console.log(body);
 	if (body && body.data && body.data.product.id) {
 		let averageRating = 0;
 
@@ -29,19 +28,13 @@ export async function POST(request: NextRequest): Promise<Response> {
 			return acc + edge.rating;
 		}, 0);
 
-		console.log(total);
-		console.log(reviewsConnection.aggregate.count);
-		averageRating = Math.floor(
-			total / reviewsConnection.aggregate.count,
-		);
-
-		console.log(averageRating);
+		averageRating = total / reviewsConnection.aggregate.count;
 
 		const averageRatingResponse = await updateProductReviewRating(
 			body.data.product.id,
 			averageRating,
 		);
-		console.log(averageRatingResponse);
+
 		return NextResponse.json(
 			{ message: `Status: ${averageRatingResponse?.id}` },
 			{ status: 201 },
